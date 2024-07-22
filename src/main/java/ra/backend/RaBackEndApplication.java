bcf4c293caec622e98b908c717adf7e7ba69050e
GabsLopes055 <lopesgabriel055@gmail.com>
2024-07-22 14:48:11 -0300

    Enviando o projeto

---
+++ src/main/java/ra/backend/RaBackEndApplication.java
@@ -0,0 +1,13 @@
+package ra.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ra/backend/RaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
