1626aadca322e623ea061d2b815a4ff20bc16a32
GabsLopes055 <lopesgabriel055@gmail.com>
2024-09-12 07:39:37 -0300

    realizando pull

--- src/main/java/ra/backend/entity/VendaEntity.java
+++ src/main/java/ra/backend/entity/VendaEntity.java
@@ -35,4 +35,72 @@ public class VendaEntity {
         dataVenda = LocalDateTime.now();
     }
 
+    public VendaEntity() {
+    }
+
+    public VendaEntity(String idVenda, List<ProdutosEntity> produtosVendidos, Double totalVenda, LocalDateTime dataVenda, MetodoPagamento metodoPagamento, StatusVenda status, Double desconto) {
+        this.idVenda = idVenda;
+        this.produtosVendidos = produtosVendidos;
+        this.totalVenda = totalVenda;
+        this.dataVenda = dataVenda;
+        this.metodoPagamento = metodoPagamento;
+        this.status = status;
+        this.desconto = desconto;
+    }
+
+    public String getIdVenda() {
+        return idVenda;
+    }
+
+    public void setIdVenda(String idVenda) {
+        this.idVenda = idVenda;
+    }
+
+    public List<ProdutosEntity> getProdutosVendidos() {
+        return produtosVendidos;
+    }
+
+    public void setProdutosVendidos(List<ProdutosEntity> produtosVendidos) {
+        this.produtosVendidos = produtosVendidos;
+    }
+
+    public Double getTotalVenda() {
+        return totalVenda;
+    }
+
+    public void setTotalVenda(Double totalVenda) {
+        this.totalVenda = totalVenda;
+    }
+
+    public LocalDateTime getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(LocalDateTime dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    public MetodoPagamento getMetodoPagamento() {
+        return metodoPagamento;
+    }
+
+    public void setMetodoPagamento(MetodoPagamento metodoPagamento) {
+        this.metodoPagamento = metodoPagamento;
+    }
+
+    public StatusVenda getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusVenda status) {
+        this.status = status;
+    }
+
+    public Double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(Double desconto) {
+        this.desconto = desconto;
+    }
 }
